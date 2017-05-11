f8b90aa68f85903ad7141b31b24d7cfe2da2fccf
Ged Lawrenson <GedLawrenson@users.noreply.github.com>
2017-05-11 23:34:50 +0100

    Barebones CLI

--- checkapache/cli.py
+++ checkapache/cli.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import click
+import argparse
+
+
+def _display_menu():
+    print 'TBC'
+
+
+def _run_update():
+    return ('TBC')
+    # Backup the file
+    # Do the replacements
+    # Inform user of backup + new file name
 
 
 @click.command()
-def main(args=None):
+@click.option('--file', help='Full path to an Apache httpd.conf file.')
+@click.option('--update', is_flag=True, help='Generate a new config file?')
+def main(file, update):
     """Console script for checkapache"""
-    click.echo("Replace this message by putting your code into "
-               "checkapache.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    _display_menu()
+    # click.echo("Put code into checkapache.cli.main")
+    # click.echo("See click documentation at http://click.pocoo.org/")
+    if update:
+        _run_update()
 
 
 if __name__ == "__main__":
